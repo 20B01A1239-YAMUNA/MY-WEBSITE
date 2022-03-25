5a646806a55a1ed3529163387c37127ff056352b
20B01A1239-YAMUNA <20b01a1239@svecw.edu.in>
2022-03-25 20:32:44 +0530

    This is my first website

---
+++ script.js
@@ -0,0 +1,27 @@
+let a=document.querySelectorAll('address');
+let b=document.querySelectorAll('email');
+let n=document.querySelectorAll('names');
+let p=document.querySelectorAll('phone');
+    
+function callme()
+{
+   if((names.value.length) <= 1)
+   {
+      alert("invalid name");
+   }  
+    if((address.value.length) === 0)
+     {
+        alert("enter the address");
+     }
+   
+     if(email.value.length === 0)
+     {
+        alert("enter mail");
+    }
+    if((phone.value != 10))
+    {
+        alert("invalid number");
+    }
+    
+    
+    }
